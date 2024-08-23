22324304f5759a2cc058d9c5f37fa19967ecf55d
goutham rangarajan <gouthamrangarajan@gouthams-MBP.fios-router.home>
2024-08-23 12:18:47 -0400

    adding a data folder to check if deployment works

--- todo/main.py
+++ todo/main.py
@@ -1,6 +1,6 @@
 from fasthtml.common import *
 
-app,rt,todos,Todo=fast_app('todo.db',todos=dict(id=int, item=str, done=bool, pk='id'))
+app,rt,todos,Todo=fast_app('data/todo.db',todos=dict(id=int, item=str, done=bool, pk='id'))
 
 def pico_css_link():
     return Link("",rel="stylesheet",href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css")
