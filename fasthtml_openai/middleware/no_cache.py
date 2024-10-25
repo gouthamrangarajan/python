9eaf8b4a6f88d61dbdc63f1eafc56042f0a33c1b
goutham rangarajan <gouthamrangarajan@gouthams-MacBook-Pro.local>
2024-10-25 09:02:43 -0400

    removed unnecessary print

--- fasthtml_openai/middleware/no_cache.py
+++ fasthtml_openai/middleware/no_cache.py
@@ -3,8 +3,7 @@ from starlette.middleware.base import BaseHTTPMiddleware
 class NoCacheMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        if request.url.path=='/assets/app.js' or request.url.path=='/assets/app.css':
-            print('enter',request.url.path)
+        if request.url.path=='/assets/app.js' or request.url.path=='/assets/app.css':            
             response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
             response.headers["Pragma"] = "no-cache"
             response.headers["Expires"] = "0"
